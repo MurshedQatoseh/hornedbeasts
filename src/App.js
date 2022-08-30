a73caf7cee600d90f382f92552c9e2e37ec81aac
Murshed Majdy Qatoseh <morshedmorahed01@gmail.com>
2022-08-30 09:59:34 +0300

    lab01

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main';
+
+class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header />
+                <div className="cards">   
+                <Main />
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App;
