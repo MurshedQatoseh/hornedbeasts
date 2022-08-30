271d3951fa167d2ba5507d972cae18afdc91802c
Murshed Majdy Qatoseh <morshedmorahed01@gmail.com>
2022-08-30 10:29:18 +0300

    lab03

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,37 +1,34 @@
-import React from "react";
-import Hornedbeast from "./HornedBeast";
-import data from '../data.json'
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import HornedBeast from './HornedBeast.js';
+import React from 'react';
+
+import Card from 'react-bootstrap/Card'
 
 
-class Main extends React.Component{
 
-    render(){
 
-        return(
-            <Row xs={1} md={2} className="g-4">
-            {
-                data.map(item =>
-                    
-
-                    <Col>
-                    
-
-                    <Hornedbeast title={item.title} imageURL={item.image_url} description={item.description}/>
-                    </Col>
-                
-                    
-                    
-                    
-                    )
-                   
-
-            
-            }
-            
-            </Row>
-        )
-    }
+class Main extends React.Component {  
+  render() {
+    return (
+      <div>
+        
+        <Card> 
+          {this.props.beasts.map((thisBeast, index) => {
+            return (
+              <div key={index}>
+                <HornedBeast
+                  showModal = {this.props.showModal}
+                  title={thisBeast.title}
+                  img={thisBeast.image_url}
+                  description={thisBeast.description}
+                  horns={thisBeast.horns}
+                />
+              </div>
+            )
+          })}
+        </Card>
+      </div>
+    )
+  }
 }
+
 export default Main;
